87c18748a836dd425dd697f866d7dcc61a062927
Ketevan Nanuashvili <ketinanuashvili@gmail.com>
2025-04-25 00:31:41 +0400

    some changes

--- Auth/Infrastructure/IdentityDbContext.cs
+++ Auth/Infrastructure/IdentityDbContext.cs
@@ -4,18 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Project.Identity
 {
-    public class ApplicationUser : IdentityUser
-    {
-      
-    }
+    public class ApplicationUser : IdentityUser { }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
     }
-
-
 }
-
--- Auth/TokenService.cs
+++ Auth/TokenService.cs
@@ -3,46 +3,45 @@ using Project.Identity;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Shared.Commands.AuthCommands;
 using Microsoft.Extensions.Configuration;
 
-namespace Project.Business.Services;
-public class TokenService
+namespace Project.Business.Services
 {
-
-
-    private readonly IConfiguration _configuration;
-
-    public TokenService(IConfiguration configuration)
+    public class TokenService
     {
-        _configuration = configuration;
-    }
-    public string GenerateToken(ApplicationUser user, IList<string> roles)
-    {
-        var secretKey = _configuration["Jwt:Key"];
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
-        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        private readonly IConfiguration _configuration;
 
-        var claims = new List<Claim>
-    {
-        new Claim(ClaimTypes.Name, user.UserName),
-        new Claim(ClaimTypes.NameIdentifier, user.Id),
-    };
-
-        if (user.Email == "ketinanuashvili@gmail.com")
+        public TokenService(IConfiguration configuration)
         {
-            claims.Add(new Claim(ClaimTypes.Role, "Admin"));
+            _configuration = configuration;
         }
 
-        var token = new JwtSecurityToken(
-            issuer: "TestIssuer",  
-            audience: "TestAudience",  
-            claims,
-            expires: DateTime.UtcNow.AddHours(1),
-            signingCredentials: credentials
-        );
+        public string GenerateToken(ApplicationUser user, IList<string> roles)
+        {
+            var secretKey = _configuration["Jwt:Key"];
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-        return new JwtSecurityTokenHandler().WriteToken(token);
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims,
+                expires: DateTime.UtcNow.AddHours(1),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
-
 }
